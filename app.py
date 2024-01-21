aae11364ffd128a5c52d05859e1410133b5d8db8
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2024-01-21 20:25:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,31 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 import streamlit as st
-import os
-import json
-import PyPDF2 as pdf
 import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+from dotenv import load_dotenv
+import json
+
+load_dotenv() ## load all our environment variables
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_gemini_response(input,pdf_content,prompt):
+def get_gemini_repsonse(input):
     model=genai.GenerativeModel('gemini-pro')
     response=model.generate_content(input)
     return response.text
 
 def input_pdf_text(uploaded_file):
-    if uploaded_file is not None:
-        reader = pdf.PdfReader(uploaded_file)
-        text=""
-        for page in range(len(reader.pages)):
-            page=reader.pages[page]
-            text+=str(page.extract_text())
-        return text
-    else:
-        raise FileNotFoundError("No file uploaded")
+    reader=pdf.PdfReader(uploaded_file)
+    text=""
+    for page in range(len(reader.pages)):
+        page=reader.pages[page]
+        text+=str(page.extract_text())
+    return text
 
-## Streamlit App
+#Prompt Template
 
-st.set_page_config(page_title="ATS Resume EXpert")
-st.header("ATS Tracking System")
-input_text=st.text_area("Job Description: ",key="input")
-uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
-
-
-if uploaded_file is not None:
-    st.write("PDF Uploaded Successfully")
-
-
-submit1 = st.button("Tell Me About the Resume")
-
-#submit2 = st.button("How Can I Improvise my Skills")
-
-submit3 = st.button("Percentage match")
-
-input_prompt1 = """
- Hey Act Like a skilled or very experience ATS(Application Tracking System)
+input_prompt="""
+Hey Act Like a skilled or very experience ATS(Application Tracking System)
 with a deep understanding of tech field,software engineering,data science ,data analyst
 and big data engineer. Your task is to evaluate the resume based on the given job description.
 You must consider the job market is very competitive and you should provide 
@@ -58,27 +39,16 @@ I want the response in one single string having the structure
 {{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
 """
 
+## streamlit app
+st.title("Smart ATS")
+st.text("Improve Your Resume ATS")
+jd=st.text_area("Paste the Job Description")
+uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
 
-input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
-the job description. First the output should come as percentage and then keywords missing and last final thoughts.
-"""
+submit = st.button("Submit")
 
-if submit1:
+if submit:
     if uploaded_file is not None:
         text=input_pdf_text(uploaded_file)
-        response=get_gemini_response(input_prompt1)
-        st.subheader("The Repsonse is")
-        st.write(response)
-    else:
-        st.write("Please upload the resume")
-
-elif submit3:
-    if uploaded_file is not None:
-        text=input_pdf_text(uploaded_file)
-        response=get_gemini_response(input_prompt3)
-        st.subheader("The Repsonse is")
-        st.write(response)
-    else:
-        st.write("Please upload the resume")
+        response=get_gemini_repsonse(input_prompt)
+        st.subheader(response)
